cdeb630847fd2dbc91b17d2203861d5d92044b5d
manuel salvi <guest.salvi.m@igt.com>
2020-08-04 15:14:15 +0200

    fix: excluded datasourceautoconfiguration, removed servletinit class for compactness

--- src/main/java/it/serp1co/repo/multidatasource/MultipleDatasourcesSpringBootApplication.java
+++ src/main/java/it/serp1co/repo/multidatasource/MultipleDatasourcesSpringBootApplication.java
@@ -2,9 +2,17 @@ package it.serp1co.repo.multidatasource;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
-public class MultipleDatasourcesSpringBootApplication {
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class MultipleDatasourcesSpringBootApplication extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MultipleDatasourcesSpringBootApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MultipleDatasourcesSpringBootApplication.class, args);
